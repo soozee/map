f08b42c26c8ca1388402b4798287d79f8c5b0060
Suz Campbell <suz.blandon@gmail.com>
2017-05-15 12:57:01 -0600

    fixing the quotations

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,27 @@ var gulp = require("gulp");
 gulp.task("autoprefixer", function() {
 	var postcss = require("gulp-postcss");
 	var sourcemaps = require("gulp-sourcemaps");
-	var autoprefixer = require("autoprefixer");
+	var watch = require("gulp-watch");
+	var batch = require("gulp-batch");
+gulp.task("watch", function() {
+	watch(
+		"*.css",
+		batch(function(events, done) {
+			gulp.start("autoprefixer", done);
+		})
+	);
+});
+
+ autoprefixer = require("autoprefixer");
 
 	return gulp
 		.src("style.css")
 		.pipe(sourcemaps.init())
 		.pipe(postcss([autoprefixer()]))
 		.pipe(sourcemaps.write("."))
-		.pipe(gulp.dest(“./dist"));
+		.pipe(gulp.dest("./dist"));
 });
 gulp.task("default", function() {
 	// place code for your default task here
 });
+
